eba0df3b3d9692c879866fae11fd54c6b308942b
ashu765k <ashutoshkhandai6156@gmail.com>
2023-12-29 19:45:42 +0530

    Using Html,Css,JavaScript

---
+++ script.js
@@ -0,0 +1,37 @@
+/*===============toggle icon navbar================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+}
+
+
+
+
+
+/*===============scorll section active link================*/
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () =>{
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+/*===============sticky navbar================*/
+let header = document.querySelector('header');
+header.classList.toggle('sticky',window.scrollY > 100);
+
+};
